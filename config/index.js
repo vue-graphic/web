20cc51bf961893572e0f591a0096e20255264d12
waheng chang <wahengchang@gmail.com>
2018-08-18 17:21:03 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'web/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/web',
 
     /**
      * Source Maps
